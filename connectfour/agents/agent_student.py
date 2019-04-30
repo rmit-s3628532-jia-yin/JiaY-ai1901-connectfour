c21ade4c6254384b38ba1d914566df84fc2d65b4
Jia Y <s3628532@student.rmit.edu.au>
2019-04-30 10:50:31 +1000

    added control center heuristic

--- connectfour/agents/agent_student.py
+++ connectfour/agents/agent_student.py
@@ -87,6 +87,66 @@ class StudentAgent(RandomAgent):
             next_state(turn)
             winner()
         """
-				
-        return random.uniform(0, 1)
+        heuristic = 0
+
+        #   put tokens in the center column as much as possible
+        heuristic += self.control_center(board)
+        return heuristic
+    
+    def control_center(self, board):
+        """
+        count the number of my tokens and opponent's tokens
+        the closer my tokens to the middle, the higher score it gets
+        get score for my tokens
+        lose score for opponent's tokens
+        """
+        heuristic = 0
+        center_column = 3
+        score_center_col = 5
+        score_2_4_col = 3
+        score_1_5_col = 2
+        
+        old_max, old_min, new_max, new_min = 0, 0, 0, 0
+
+        for x in range(board.height):
+            #   center column
+            if board.get_cell_value(x, center_column) == self.id:
+                heuristic += score_center_col
+            if board.get_cell_value(x, center_column) == self.id % 2 + 1:
+                heuristic -= score_center_col
+        
+            #   2nd and 4th column
+            if board.get_cell_value(x, 2) == self.id or board.get_cell_value(x, 4) == self.id:
+                heuristic += score_2_4_col
+            if board.get_cell_value(x, 2) == self.id % 2 + 1 or board.get_cell_value(x, 4) == self.id % 2 + 1:
+                heuristic -= score_2_4_col
+                
+            #   1st and 5th column
+            if board.get_cell_value(x, 1) == self.id or board.get_cell_value(x, 5) == self.id:
+                heuristic += score_1_5_col
+            if board.get_cell_value(x, 1) == self.id % 2 + 1 or board.get_cell_value(x, 5) == self.id % 2 + 1:
+                heuristic -= score_1_5_col
+        
+        """
+        fit the value between -1 and 1
+        """
+        old_max = score_center_col * board.height + score_2_4_col * board.height * 2.0 + score_1_5_col * board.height * 2.0
+        old_min = 0 - old_max
+        new_max = 1.0
+        new_min = -1.0
+        
+        value = self.fit_range(old_max, old_min, new_max, new_min, heuristic)
+
+        return value
+
+    def fit_range(self, old_max, old_min, new_max, new_min, old_value):
+        """
+        fit number range to range from -1 to 1
+        """
+        
+        old_range = old_max - old_min
+        new_range = new_max - new_min
+        new_value = (((old_value - old_min) * new_range) / old_range) + new_min
+
+        return new_value
 
